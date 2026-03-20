91d2ff353ab5fdd148c22838943588802beafaa7
jubyte <julia_ferreira3@estudante.sesisenai.org.br>
2026-03-20 09:08:01 -0300

    add - exercicio 9

---
+++ exercicio9.js
@@ -0,0 +1,11 @@
+function ordemNum2() {
+    let a = Number(prompt("Digite o primeiro número:"));
+    let b = Number(prompt("Digite o segundo número:"));
+
+    if (a > b) {
+        console.log(a + ", " + b);
+    } else {
+        console.log(b + ", " + a);
+    }
+}
+
