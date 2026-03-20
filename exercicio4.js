afe2dc5e7ad0af755f365b60446212348daac2ee
jubyte <julia_ferreira3@estudante.sesisenai.org.br>
2026-03-20 09:07:18 -0300

    add - exercicio 4

---
+++ exercicio4.js
@@ -0,0 +1,11 @@
+function inverte() {
+    let numeros = [];
+
+    for (let i = 0; i < 4; i++) {
+        numeros.push(Number(prompt("Digite um número:")));
+    }
+
+    numeros.sort((a, b) => b - a);
+
+    alert("Ordem decrescente: " + numeros);
+}
