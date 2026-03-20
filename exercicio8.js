3364dbd4343da034d8bdbc664338669a6afb6f3c
jubyte <julia_ferreira3@estudante.sesisenai.org.br>
2026-03-20 09:07:55 -0300

    add - exercicio 8

---
+++ exercicio8.js
@@ -0,0 +1,8 @@
+function conta() {
+    let a = 10;
+    let b = 5;
+
+    console.log("Diferença: " + (a - b));
+    console.log("Dobro da A + Triplo da B: " + (a * 2 + b * 3));
+    console.log("Multiplicação: " + (a * b));
+}
