265e961a0a5970107075753e17919756a7bd0f68
jubyte <julia_ferreira3@estudante.sesisenai.org.br>
2026-03-20 09:06:28 -0300

    fix - arrumei o link do ex1

---
+++ exercicio1.js
@@ -0,0 +1,46 @@
+// console.log ("Hello World");
+
+// let nome = "Julia";
+// console.log (nome);
+
+// const pi = 3.14;
+// console.log (pi);
+
+// let n1 = 1;
+// let n2 = 2;
+
+// console.log (n1 + n2);
+
+// function somar (n3, n4){
+//     console.log (n3 + n4);
+// };
+
+// somar(1,1);
+
+// function saudacao(){
+//     console.log ("Olá meu nome é " + nome);
+// };
+
+// saudacao();
+
+function somarNumeros() {
+    let soma = 0;
+    let continuar = true;
+
+    while(continuar){
+        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar): "));
+
+        // soma = soma + numero;
+
+        if (isNaN(numero)){
+            alert("Por favor, digite um número válido!");
+        } else {
+            soma += numero;
+        }
+
+        continuar = confirm("Deseja adicionar mais um número.");
+    }
+    alert ("A soma dos números é: " + soma);
+}
+
+somarNumeros();
